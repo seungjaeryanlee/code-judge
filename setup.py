eb1e2671a6271c6a93c4d73857271120fa405e6b
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-07-13 02:17:38 +0900

    Add minimal example for PyPI package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='code-judge',
+      version='0.0.0',
+      description='Offline code judge system for coding interview questions',
+      url='https://github.com/seungjaeryanlee/code-judge',
+      author='Seungjae Ryan Lee',
+      author_email='seungjaeryanlee@gmail.com',
+      license='MIT',
+      packages=['judge'],
+      zip_safe=False)
