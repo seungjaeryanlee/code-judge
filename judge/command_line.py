2a44eb387d855f238de657c29423772c65e8cd87
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-07-13 02:46:25 +0900

    Implement basic evaluation for example problem

--- judge/command_line.py
+++ judge/command_line.py
@@ -1,5 +1,23 @@
+import importlib
+import sys
+
 import judge
 
 
 def evaluate():
-    return 'Evaluating function...'
+    filepath = sys.argv[1]
+    folderpath, filename = '/'.join(filepath.split('/')[:-1]), '.'.join(filepath.split('/')[-1].split('.')[:-1])
+    sys.path.insert(0, folderpath)
+
+    solution_module = importlib.import_module(filename)
+    solution_function = solution_module.solution
+
+    try:
+        answer = solution_function()
+    except:
+        print('[FAIL] An exception occurred.')
+    else:
+        if answer == 233168:
+            print('[SUCCESS] Correct answer: 233168')
+        else:
+            print('[SUCCESS] The answer {} is incorrect'.format(answer))
